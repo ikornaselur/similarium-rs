ac994e92356038027442914c64b7c25ab7a08748
Axel Örn Sigurðsson <dev@absalon.is>
2022-11-13 19:31:58 +0000

    Implement Channel model

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,8 +1,10 @@
+mod channel;
 mod game;
 mod nearby;
 mod similarity_range;
 mod word2vec;
 
+pub use channel::Channel;
 pub use game::Game;
 pub use nearby::Nearby;
 pub use similarity_range::SimilarityRange;
