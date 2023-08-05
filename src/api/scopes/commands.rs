ec3c339d0cece0d0f92ade6aee67e9d0cb63429f
Axel Örn Sigurðsson <dev@absalon.is>
2023-08-05 18:57:00 +0100

    Start handling supported commands

--- src/api/scopes/commands.rs
+++ src/api/scopes/commands.rs
@@ -1,4 +1,5 @@
 use crate::api::app::AppState;
+use crate::api::utils::{parse_command, Command};
 use crate::models::slack_bot::get_slack_bot_token;
 use crate::payloads::CommandPayload;
 use crate::SimilariumError;
@@ -12,24 +13,32 @@ async fn post_similarium_command(
     log::debug!("POST /slack/similarium");
 
     let payload = form.into_inner();
-
     let token = get_slack_bot_token(&payload.team_id, &payload.api_app_id, &app_state.db).await?;
+    let command = parse_command(&payload.text)?;
 
-    match payload.text.to_lowercase().trim() {
-        "help" => {
+    match command {
+        Command::Help => {
             app_state
                 .slack_client
                 .post_message("Help text", &payload.channel_id, &token)
                 .await?;
         }
-        _ => {
+        Command::Start(time) => {
             app_state
                 .slack_client
                 .post_message(
-                    &format!("Hey <@{}>! You said: {}", payload.user_id, payload.text),
+                    &format!("Starting the game at {}", time.format("%H:%M")),
                     &payload.channel_id,
                     &token,
                 )
+                .await?;
+        }
+        Command::ManualStart | Command::ManualEnd => todo!(),
+        Command::Stop => todo!(),
+        Command::Invalid(message) => {
+            app_state
+                .slack_client
+                .post_message(&message, &payload.channel_id, &token)
                 .await?;
         }
     }
